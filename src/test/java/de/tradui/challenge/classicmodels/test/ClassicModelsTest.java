4bbd412b72d5e58cdb8093bde20ec2facd0f4499
TRADUI\c.kaddatz <c.kaddatz@tradui.de>
2018-09-11 11:58:47 +0200

    Initialize project

---
+++ src/test/java/de/tradui/challenge/classicmodels/test/ClassicModelsTest.java
@@ -0,0 +1,34 @@
+package de.tradui.challenge.classicmodels.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author c.kaddatz
+ */
+public class ClassicModelsTest {
+    
+    public ClassicModelsTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testClassicModelsApp() {
+        //TODO test...
+    }
+}
